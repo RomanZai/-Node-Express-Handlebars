87924aa1c528b548a72fc8cf6ec796e52ee7d09a
Roman <rom.zaikin@gmail.com>
2019-10-30 00:06:57 -0700

    final+Readme+heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burger_db"
-});
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burger_db"
+    })
+
+    }
 
 const connectionPromise = new Promise((resolve, reject) => {
     connection.connect(function (err) {
